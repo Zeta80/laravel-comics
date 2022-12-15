9f9e81fdf61b016a940933cb2c9f549a7fdefd1c
Mirko Robbi <mirko.robbi@gmail.com>
2022-12-15 16:43:10 +0100

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,150 @@
+<div class="footer-wrapper">
+    <section class="blu-row">
+        <div class="stack-wrapper">
+            <div class="row row-cols-5 flex-row">
+                <div class="col d-flex flex-row">
+                    <div class="img">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    </div>
+                    <div class="text">
+                        <p>DIGITAL COMICS</p>
+                    </div>
+                </div>
+                <div class="col d-flex flex-row">
+                    <div class="img">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    </div>
+                    <div class="text">
+                        <p>DIGITAL COMICS</p>
+                    </div>
+                </div>
+                <div class="col d-flex flex-row">
+                    <div class="img">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    </div>
+                    <div class="text">
+                        <p>DIGITAL COMICS</p>
+                    </div>
+                </div>
+                <div class="col d-flex flex-row">
+                    <div class="img">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    </div>
+                    <div class="text">
+                        <p>DIGITAL COMICS</p>
+                    </div>
+                </div>
+                <div class="col d-flex flex-row">
+                    <div class="img">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    </div>
+                    <div class="text">
+                        <p>DIGITAL COMICS</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="footer-links">
+        <div class="links-wrapper">
+            <div class="contact-links">
+                <div>
+                    <h2>DC COMICS </h2>
+                    <ul>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                    </ul>
+                    <h2>SHOP </h2>
+                    <ul>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>DC </h2>
+                    <ul>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>SITES</h2>
+                    <ul>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                        <li>
+                            <a href=""> lorem </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="links-img">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="footer-social">
+        <div class="wrapper-social">
+            <div class="button">
+                <button>SIGN-UP NOW!</button>
+            </div>
+            <div class="social-icon">
+                <h3>FOLLOW US</h3>
+                <span><i class="fa-brands fa-facebook-f"></i></span>
+                <span><i class="fa-brands fa-twitter"></i></span>
+                <span><i class="fa-brands fa-youtube"></i></span>
+                <span><i class="fa-brands fa-pinterest-p"></i></span>
+                <span><i class="fa-solid fa-location-dot"></i></span>
+            </div>
+        </div>
+    </section>
+</div>
