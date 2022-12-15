820a38e252bdba56ccce5ae04d4e5a842bbee005
Mirko Robbi <mirko.robbi@gmail.com>
2022-12-15 15:36:32 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Ciao sono una pagina home</h1>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,47 @@
+<div class="background-white">
+    <div class="my_container">
+
+        <header class="header d-flex justify-content-around">
+            <div class="logo">
+                <img src=" {{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo dc">
+            </div>
+            <nav class="header-navigation ">
+                <ul class="d-flex flex-row">
+                    <li>
+                        <a>CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a>COMICS</a>
+                    </li>
+                    <li>
+                        <a>MOVIES</a>
+                    </li>
+                    <li>
+                        <a>TV</a>
+                    </li>
+                    <li>
+                        <a>GAMES</a>
+                    </li>
+                    <li>
+                        <a>COLLECTIONS</a>
+                    </li>
+                    <li>
+                        <a>VIDEOS</a>
+                    </li>
+                    <li>
+                        <a>FANS</a>
+                    </li>
+                    <li>
+                        <a>NEWS</a>
+                    </li>
+                    <li>
+                        <a>SHOP</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <div class="banner">
+            <img src="{{ VITE::asset('resources/img/jumbotron.jpg') }}" alt="">
+        </div>
+    </div>
+</div>
